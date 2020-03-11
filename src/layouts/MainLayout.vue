b842647efbacd000c65e396bb52660098e884637
Lucas Vieira <lucas4506@live.com>
2020-03-11 16:58:51 -0300

    Quarta-16:57

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,112 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+      </q-toolbar>
+      <div class="q-px-lg q-pt-xl q-mb-md">
+        <div class="text-h3">Biblioteca</div>
+        <div class="subtitle1">{{ dataAtual }}</div>
+      </div>
+
+      <q-img
+        src="statics/biblioteca2.png"
+        class="header-image absolute-top" />
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+        show-if-above
+        :width="250"
+        :breakpoint="600"
+      >
+        <q-scroll-area style="height: calc(100% - 185px); margin-top: 185px; border-right: 1px solid #ddd">
+          <q-list padding>
+            <q-item exact
+            to = "/"
+            clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="list" />
+              </q-item-section>
+
+              <q-item-section>
+                Lista de Livros
+              </q-item-section>
+            </q-item>
+
+            <q-item to = "/help" clickable exact v-ripple>
+              <q-item-section avatar>
+                <q-icon name="help" />
+              </q-item-section>
+
+              <q-item-section>
+                Ajuda
+              </q-item-section>
+            </q-item>
+
+            <q-item to = "/Error404" clickable exact v-ripple>
+              <q-item-section avatar>
+                <q-icon name="group" />
+              </q-item-section>
+
+              <q-item-section>
+                Ajuda
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-scroll-area>
+
+        <q-img class="absolute-top" src="https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_960_720.jpg" style="height: 185px ">
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+            <div class="text-weight-bold">Lucas Vieira</div>
+            <div>@Lobus18</div>
+          </div>
+        </q-img>
+    </q-drawer>
+
+    <q-page-container>
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { date } from 'quasar'
+
+export default {
+  name: 'MainLayout',
+
+  data () {
+    return {
+      leftDrawerOpen: false,
+    }
+  },
+  computed:{
+    dataAtual(){
+      let timeStamp = Date.now()
+      return date.formatDate(timeStamp, 'dddd, DD/MMMM/YYYY | 20ºC')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .header-image{
+    height: 100%;
+    z-index: -1;
+    opacity: 0.55;
+    filter: grayscale(50%);
+  }
+</style>
